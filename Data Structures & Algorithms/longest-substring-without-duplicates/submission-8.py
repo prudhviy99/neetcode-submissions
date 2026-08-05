9aef0b4b76cac06f494e47d328e17a8090e84336
Prudhvi <52329225+prudhviy99@users.noreply.github.com>
2026-08-05 11:31:15 -0700

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # left pointer starts at 0
+        # we use a hashmap to store all indexes
+        # we use right pointer to iterate
+        # check if hashmap has r value while iterating r
+        # if duplicate exists, move l to the last index of r value + 1  
+        # ( which we just found a     duplicate)
+        
+        hmap = {}
+        l = 0
+        res = 0
+        
+        for r in range(len(s)):
+            if s[r] in hmap:
+                l = max(hmap[s[r]] + 1, l)
+            hmap[s[r]] = r
+            res = max(res, r - l + 1)
+        
+        return res
+            
